3f09924bba35289adb6e3c789f270b84ec671306
Alex Aristov <nightfire91@list.ru>
2022-10-02 09:32:34 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // случайное целое число из диапазона от 1 до 10 (1 2 3 4 ... 9) (от min до max-1)
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
