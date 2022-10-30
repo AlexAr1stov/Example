19462e0de700f21858bdfdfd3db548ed3d7b329a
Alex Aristov <nightfire91@list.ru>
2022-10-30 18:41:53 +0300

    Seminar_2_task1(Max_Cifra)_task2(Delete_Cifri)_task3(Kratnost')

---
+++ Seminar2_Task2(11)_Delete_cifri/Program.cs
@@ -0,0 +1,17 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100,1000);
+int FirstSymbol = number/100;
+int LastSymbol = number%100;
+Console.WriteLine($"Число {number} сократилось до {FirstSymbol}{LastSymbol}");
+
+// int num = new Random().Next(100,1000);
+// Console.WriteLine(num);
+// int Three = num % 10;
+// int Two = (num / 10) % 10;
+// int One = num / 100;
+// int NewNum = One*10+Three;
+// Console.WriteLine($"Число без второй цифры {NewNum}");
---
+++ Seminar2_Task3(12)_Kratnost/Program.cs
@@ -0,0 +1,24 @@
+﻿// 12. Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите 1 число: ");
+int NumberOne = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int NumberTwo = int.Parse(Console.ReadLine());
+if (NumberOne ==0)
+{
+    Console.WriteLine("Ошибка, введен 0");
+    return;
+}
+
+int ostatok = NumberTwo % NumberOne;
+if (ostatok == 0)
+{
+    Console.WriteLine($"{NumberTwo}, {NumberOne}-> кратно ");
+}
+else
+{
+    Console.WriteLine($"{NumberTwo}, {NumberOne}-> не кратно, остаток {ostatok} ");
+}
