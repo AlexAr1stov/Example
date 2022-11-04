6094e74bb5f81befaf29e4fd75ce62dd22d0367f
AlexAr <nightfire91@list.ru>
2022-11-04 19:54:29 +0300

    Home_Work_2_002_Task_13_3cifra_number

--- Home_Work2_002(Task13)/Program.cs
+++ Home_Work2_002(Task13)/Program.cs
@@ -5,3 +5,23 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int Three=0;
+int AbsNumber=Math.Abs(number);
+if (AbsNumber < 100)
+{
+    Console.WriteLine($"В числе {number} третьей цифры нет");
+}
+else 
+if (AbsNumber >= 100)
+{
+    while (AbsNumber > 1000)
+    {
+    AbsNumber = AbsNumber/ 10;
+    }
+    Three = AbsNumber % 10;
+    Console.WriteLine($"В числе третья цифра {Three}");
+}
+
