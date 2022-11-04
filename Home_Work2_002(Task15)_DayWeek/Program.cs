6a23175aa5a5a0df697fe15ee99fc2fd9e2e9cef
AlexAr <nightfire91@list.ru>
2022-11-04 17:30:52 +0300

    Home_Work_2_003_Task15_DayWeek

---
+++ Home_Work2_002(Task15)_DayWeek/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите цифру дня недели: ");
+int NumberDay = int.Parse(Console.ReadLine());
+if (NumberDay > 0 && NumberDay < 8)
+{
+    if (NumberDay == 6 || NumberDay == 7)
+    {
+        Console.WriteLine($"Урра! {NumberDay} день по счету, это выходной день!");
+    }
+    else 
+    if (NumberDay <= 5)
+    {
+        Console.WriteLine($"{NumberDay} день по счету, это будний день!");
+    }
+}
+else
+Console.WriteLine("Укажите корректный день недели! От 1 до 7");
