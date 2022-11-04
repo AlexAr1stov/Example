bb116853c05b6e46cebdc664647d91b15de2e552
AlexAr <nightfire91@list.ru>
2022-11-04 18:12:40 +0300

    Home_Work_5_001_Task34

--- Home_Work5_001(Task34)/Program.cs
+++ Home_Work5_001(Task34)/Program.cs
@@ -3,3 +3,32 @@
 
 // [345, 897, 568, 234] -> 2
 
+Console.WriteLine("Введите размер массива:  ");
+int size = int.Parse(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, количество четных - {count}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int j = 0; j < numbers.Length; j++)
+    {
+        numbers[j] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for(int j = 0; j < numbers.Length; j++)
+    {
+        Console.Write(numbers[j] + " ");
+    }
+    Console.WriteLine();
+}
